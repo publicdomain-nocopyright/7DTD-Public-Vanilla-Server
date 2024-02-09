251c3183ce1b02e39b5231aee59ab5c19459e3a9
Feldwor <BoQsc@users.noreply.github.com>
2024-02-09 10:09:02 +0200

    Update 7DTD_Vanilla_Server.cmd

--- launcher/windows/7DTD_Vanilla_Server.cmd
+++ launcher/windows/7DTD_Vanilla_Server.cmd
@@ -3,6 +3,19 @@ ECHO Receiving 7 Days To Die Public Vanilla Server information
 CURL "https://raw.githubusercontent.com/publicdomain-nocopyright/ip-records/main/7dtd-public-vanilla-server.txt" > "%TEMP%\7dtd-public-vanilla-server.txt"
 SET /P Server_IP= < "%TEMP%\7dtd-public-vanilla-server.txt"
 
+:reconnect
+curl -s 51.178.20.227:62550  
+IF ERRORLEVEL 7 (
+	
+	ECHO 7DTD Vanilla Server is offline. 
+	ECHO Trying to reconnect in...
+	TIMEOUT /t 3
+	CLS
+	GOTO :reconnect
+)
+IF ERRORLEVEL 0 ECHO Vanilla Server is Online.
+
+
 IF NOT "%Server_IP%" == "" (
 	ECHO 7 Days To Die Public Vanilla Server IP: %Server_IP%
 	ECHO You are about to try to connect to Vanilla Server
