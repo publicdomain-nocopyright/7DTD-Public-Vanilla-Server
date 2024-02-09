6df7b48911c843ef379fe736e337fbcd031b1bce
Feldwor <BoQsc@users.noreply.github.com>
2024-02-09 10:16:08 +0200

    Update 7DTD_Vanilla_Server.cmd

--- launcher/windows/7DTD_Vanilla_Server.cmd
+++ launcher/windows/7DTD_Vanilla_Server.cmd
@@ -1,4 +1,8 @@
 @ECHO OFF && TITLE 7 Days To Die Public Vanilla Server Launcher
+
+WHERE curl
+IF %ERRORLEVEL% NEQ 0 ECHO You need to download curl program for this launcher to work. && PAUSE && EXIT 
+
 ECHO Receiving 7 Days To Die Public Vanilla Server information
 CURL "https://raw.githubusercontent.com/publicdomain-nocopyright/ip-records/main/7dtd-public-vanilla-server.txt" > "%TEMP%\7dtd-public-vanilla-server.txt"
 SET /P Server_IP= < "%TEMP%\7dtd-public-vanilla-server.txt"
