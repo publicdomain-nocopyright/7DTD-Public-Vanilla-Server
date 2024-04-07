53b0430e41676cf88cee5623c74599052d4f971c
Feldwor <BoQsc@users.noreply.github.com>
2024-04-07 16:00:16 +0300

    Update start_OpenCollective_API_image_server.sh

--- OpenCollective_API_image_server/UI_ESC_2_OpenCollective_Image_Server_Display/start_OpenCollective_API_image_server.sh
+++ OpenCollective_API_image_server/UI_ESC_2_OpenCollective_Image_Server_Display/start_OpenCollective_API_image_server.sh
@@ -5,6 +5,7 @@ script_dir="$(cd "$(dirname "$0")" && pwd)"
 echo "The directory of the current script is: $script_dir"
 cd $script_dir
 # Run the Python script in the background using nohup
+python3 -m venv venv
 source venv/bin/activate
 nohup python3 OpenCollective_API_image_server.py > OpenCollective_API_image_server.log 2>&1 &
 ps aux | grep 'OpenCollective_API_image_server.py' | grep -v grep | awk '{print $2}'
