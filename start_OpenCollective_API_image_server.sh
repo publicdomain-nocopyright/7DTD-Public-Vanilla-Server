0f9a002a17cc61e9f7b572e66b8a9ce8d9cb16de
Feldwor <BoQsc@users.noreply.github.com>
2024-04-07 16:18:26 +0300

    update linux tested

---
+++ start_OpenCollective_API_image_server.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Get the directory of the script
+script_dir="$(cd "$(dirname "$0")" && pwd)"
+echo "The directory of the current script is: $script_dir"
+cd $script_dir
+# Run the Python script in the background using nohup
+python3 -m venv venv
+source venv/bin/activate
+pip install pillow==9.5.0
+pip install requests
+
+nohup python3 OpenCollective_API_image_server.py > OpenCollective_API_image_server.log 2>&1 &
+ps aux | grep 'OpenCollective_API_image_server.py' | grep -v grep | awk '{print $2}'
