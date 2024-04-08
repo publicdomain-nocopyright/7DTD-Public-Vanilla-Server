3a7acbf64bf4e2738ec596fe95d7b2dca92bd82f
Feldwor <BoQsc@users.noreply.github.com>
2024-04-08 14:24:55 +0300

    Update start_OpenCollective_API_image_server.sh

--- OpenCollective_API_image_server/UI_ESC_2_OpenCollective_Image_Server_Display/start_OpenCollective_API_image_server.sh
+++ OpenCollective_API_image_server/UI_ESC_2_OpenCollective_Image_Server_Display/start_OpenCollective_API_image_server.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+bash ./stop_OpenCollective_API_image_server.sh
 # Get the directory of the script
 script_dir="$(cd "$(dirname "$0")" && pwd)"
 echo "The directory of the current script is: $script_dir"
 cd $script_dir
 # Run the Python script in the background using nohup
 python3 -m venv venv
-source venv/bin/activate
+source ./venv/bin/activate
+
+wget https://bootstrap.pypa.io/get-pip.py && python get-pip.py
 pip install pillow==9.5.0
 pip install requests
 pip install --upgrade certifi
