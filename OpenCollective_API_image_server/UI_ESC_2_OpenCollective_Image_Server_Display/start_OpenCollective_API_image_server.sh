299395affff150bee82e369b5132227dfa1997bb
Feldwor <BoQsc@users.noreply.github.com>
2024-04-08 14:13:52 +0300

    update

--- OpenCollective_API_image_server/UI_ESC_2_OpenCollective_Image_Server_Display/start_OpenCollective_API_image_server.sh
+++ OpenCollective_API_image_server/UI_ESC_2_OpenCollective_Image_Server_Display/start_OpenCollective_API_image_server.sh
@@ -9,6 +9,7 @@ python3 -m venv venv
 source venv/bin/activate
 pip install pillow==9.5.0
 pip install requests
+pip install --upgrade certifi
 
 nohup python3 OpenCollective_API_image_server.py > OpenCollective_API_image_server.log 2>&1 &
 ps aux | grep 'OpenCollective_API_image_server.py' | grep -v grep | awk '{print $2}'
