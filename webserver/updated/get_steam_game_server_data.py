4d059b736a7e9e3e1b6721de1d3f630e5a874f40
Feldwor <BoQsc@users.noreply.github.com>
2024-06-28 16:41:12 +0300

    update

---
+++ webserver/updated/get_steam_game_server_data.py
@@ -0,0 +1,9 @@
+import a2s
+
+def get_steam_game_server_data(server_address = ("93.49.104.86", 26900)):
+    rules = a2s.rules(server_address)
+    return rules
+
+import sys
+# Replace the module with the function
+sys.modules[__name__] = get_steam_game_server_data
--- webserver/updated/test.py
+++ webserver/updated/test.py
@@ -1,3 +1,5 @@
 import get_system_info
+import get_steam_game_server_data
 
 print(get_system_info())
+print(get_steam_game_server_data())
