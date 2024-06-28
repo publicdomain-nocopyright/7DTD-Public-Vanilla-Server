4a73ab422f8d78360e65dfdf5e1edd0e285f5afd
Feldwor <BoQsc@users.noreply.github.com>
2024-06-28 17:01:52 +0300

    update

--- webserver/updated/webserver.py
+++ webserver/updated/webserver.py
@@ -1,1 +1,30 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler, ThreadingHTTPServer
+
+class RedirectHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+
+            html_content = f"""
+            test
+            """
+            self.wfile.write(html_content.encode('utf-8'))
+              
+        if self.path == '/data':
+                    self.send_response(200)
+                    self.send_header('Content-type', 'application/json')
+                    self.send_header('Access-Control-Allow-Origin', '*')  # Allow requests from any origin
+                    self.send_header('Access-Control-Allow-Methods', 'GET')
+                    self.send_header('Access-Control-Allow-Headers', 'Content-type')
+                    self.end_headers()
+
+# Function to run the HTTP server
+def run(server_class=ThreadingHTTPServer, handler_class=RedirectHandler, port=80):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print('Server started at localhost:' + str(port))
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    
+    # Run the HTTP server in the main thread
+    run()
