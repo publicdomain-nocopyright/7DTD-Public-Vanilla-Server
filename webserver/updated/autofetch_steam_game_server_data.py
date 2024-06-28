3651e2f57c9c1216df84fcd74b5e2a0e0caaa503
Feldwor <BoQsc@users.noreply.github.com>
2024-06-28 23:14:52 +0300

    Update autofetch_steam_game_server_data.py

--- webserver/updated/autofetch_steam_game_server_data.py
+++ webserver/updated/autofetch_steam_game_server_data.py
@@ -1,4 +1,4 @@
 # Webserver needs to execute steam game server data function every 20 seconds
 # This can be done by measuring time and checking if time has come to fetch again.
 # Also can be achieved by having separate thread running that executes function every 20 seconds.
-# Also can be achieved by asychronity and fetching by delay of 20 seconds.
+# Also can be achieved by asynchronity and fetching by delay of 20 seconds.
