e072a5710fde97eee23171b6c70e11365f64cdda
Feldwor <BoQsc@users.noreply.github.com>
2024-07-01 10:44:24 +0300

    Update ssl_verification.py

--- webserver/updated/ssl_verification.py
+++ webserver/updated/ssl_verification.py
@@ -31,3 +31,6 @@ def enable_ssl(httpd, server_address):
     # https://community.letsencrypt.org/t/certbot-discontinuing-windows-beta-support-in-2024/208101
     # https://certifytheweb.com/
     # https://www.win-acme.com/
+    # Key takeaways:
+    #   internal host names like: localhost are not supported.
+    #   authorisation happens through a http challenge where you open ports to the website and specify path for certifyweb to place the challenge in and authorise to make certificate.
