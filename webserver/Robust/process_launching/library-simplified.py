77add94d8d5754a00a38118c19c0558092a1c817
Feldwor <BoQsc@users.noreply.github.com>
2024-07-21 22:59:10 +0300

    Create library-simplified.py

---
+++ webserver/Robust/process_launching/library-simplified.py
@@ -0,0 +1,6 @@
+def produce_renamed_python_executable(new_executable_path: str):
+    import os, shutil, pathlib, sys
+    python_executable_installed = pathlib.Path(sys.exec_prefix, 'python.exe')  # change to pythonw to create a pythonw executable.
+    python_executable_new = pathlib.Path(new_executable_path)
+    os.makedirs(os.path.dirname(new_executable_path), exist_ok=True)
+    return str(shutil.copy2(python_executable_installed, python_executable_new))
