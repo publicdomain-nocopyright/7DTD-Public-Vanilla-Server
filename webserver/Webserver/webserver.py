586f3569ab7e9d4c072490361799561dc69977a1
Feldwor <BoQsc@users.noreply.github.com>
2024-07-30 17:27:51 +0300

    Update webserver.py

--- webserver/Webserver/webserver.py
+++ webserver/Webserver/webserver.py
@@ -9,15 +9,14 @@ class RedirectHandler(BaseHTTPRequestHandler):
 
             from pathlib import Path
             self.wfile.write(open(Path(__file__).parent / 'index.html', 'rb').read())
+
         if self.path == '/favicon.ico':
             self.send_response(200), self.send_header('Content-type', 'image/x-icon'), self.end_headers()
-
             favicon_base64 = (
                """AAABAAEAEBAQAAEABAAoAQAAFgAAACgAAAAQAAAAIAAAAAEABAAAAAAAgAAAAAAAAAAAAAAAEAAAAAAAAABXqw4AAAAAAGCuJwBmvSQAtd6YAPHw7ABpvisAccwuAAAAAAAAAAAAAAAAAAAAAA
                AAAAAAAAAAAAAAAAAAAAAAERERERERERERERJndiEREREQd3d3dwERERd3J3d3cRERd3JSd3d3ERF3JVUnd3cRE3dVVVJ3dzEXd1V0VSd3cRd3d3dFUndxF3d3d3RVd3ERd3d3d0d3ERF3d3d3d3cRE
                Rd3d3d3cREREXd3d3cRERERF3d3cRERERERERERERH//wAA+B8AAOAHAADgBwAAwAMAAMADAACAAQAAgAEAAIABAACAAQAAwAMAAMADAADgBwAA8A8AAPgfAAD//wAA
                """)
-            
             import base64        
             self.wfile.write(base64.b64decode(favicon_base64))
             
