11b616b71dca08a5bc8bb51000c95e9e7670f9fd
Feldwor <BoQsc@users.noreply.github.com>
2024-07-30 13:08:21 +0300

    update

--- webserver/Webserver/run.cmd
+++ webserver/Webserver/run.cmd
@@ -1,7 +1,9 @@
 @ECHO OFF
+python webserver.py
+pause
 REM start /B "" python webserver.py
 
 REM TODO: Run webserver under cmd and exit the whole cmd if launched from fresh cmd.
 REM Force kill the main thread.
-python webserver.py
+
 EXIT
