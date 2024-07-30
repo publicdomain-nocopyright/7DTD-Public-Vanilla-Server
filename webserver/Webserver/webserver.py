b09286dafabce7fb7f8311bef2e393bca355bbd3
Feldwor <BoQsc@users.noreply.github.com>
2024-07-30 16:54:02 +0300

    Update webserver.py

--- webserver/Webserver/webserver.py
+++ webserver/Webserver/webserver.py
@@ -6,10 +6,10 @@ class RedirectHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/':
             self.send_response(200), self.send_header('Content-type', 'text/html'), self.end_headers()
-            
-            from pathlib import Path
-            self.wfile.write(Path(__file__).parent.joinpath('index.html').read_bytes())
 
+            from pathlib import Path
+            self.wfile.write(open(Path(__file__).parent / 'index.html', 'rb').read())
+            
 def server(server_class=ThreadingHTTPServer, handler_class=RedirectHandler, ip='localhost', port=80):
     from threading import Thread
     
