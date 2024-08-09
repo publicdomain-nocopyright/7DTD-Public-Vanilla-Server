2f43fde93088256341a6f458fb6572610de7253c
Feldwor <BoQsc@users.noreply.github.com>
2024-08-09 19:32:32 +0300

    Update Webserver_game_server_log_reader.py

--- webserver/Webserver-testing/Webserver/Webserver_game_server_log_reader.py
+++ webserver/Webserver-testing/Webserver/Webserver_game_server_log_reader.py
@@ -1,4 +1,5 @@
 def get_latest_game_server_log_filepath(log_directory = None):
+    
     from pathlib import Path; 
     DEFAULT_LOG_DIRECTORY = r"C:\Program Files (x86)\Steam\steamapps\common\7 Days To Die\7DaysToDie_Data"
     RELATIVE_LOG_DIRECTORY = Path(r'..\7DaysToDie_Data').resolve()
