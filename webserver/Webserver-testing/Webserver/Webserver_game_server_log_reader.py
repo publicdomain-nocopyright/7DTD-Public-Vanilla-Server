35b1d49674f9265b7666f1c70491a48cb0924ce1
Feldwor <BoQsc@users.noreply.github.com>
2024-08-09 20:01:03 +0300

    Update Webserver_game_server_log_reader.py

--- webserver/Webserver-testing/Webserver/Webserver_game_server_log_reader.py
+++ webserver/Webserver-testing/Webserver/Webserver_game_server_log_reader.py
@@ -1,24 +1,22 @@
 # A robust standalone function for log file path
 def get_latest_game_server_log_file_path(log_directory : str = None) -> str:
-    try:
-        import module_get_game_server_log_directory
-    except ImportError:
-        from pathlib import Path; 
-        DEFAULT_LOG_DIRECTORY = r"C:\Program Files (x86)\Steam\steamapps\common\7 Days To Die\7DaysToDie_Data"
-        RELATIVE_LOG_DIRECTORY = Path(r'..\7DaysToDie_Data').resolve()
+    
+    from pathlib import Path; 
+    DEFAULT_LOG_DIRECTORY = r"C:\Program Files (x86)\Steam\steamapps\common\7 Days To Die\7DaysToDie_Data"
+    RELATIVE_LOG_DIRECTORY = Path(r'..\7DaysToDie_Data').resolve()
 
-        if log_directory == "FROM_DEFAULT_LOG_DIRECTORY":
-            log_directory = DEFAULT_LOG_DIRECTORY
+    if log_directory == "FROM_DEFAULT_LOG_DIRECTORY":
+        log_directory = DEFAULT_LOG_DIRECTORY
 
-        if log_directory == "FROM_RELATIVE_LOG_DIRECTORY":
-            log_directory = RELATIVE_LOG_DIRECTORY
+    if log_directory == "FROM_RELATIVE_LOG_DIRECTORY":
+        log_directory = RELATIVE_LOG_DIRECTORY
 
-        import os; 
-        if os.path.exists(RELATIVE_LOG_DIRECTORY) and log_directory is None: 
-            log_directory = RELATIVE_LOG_DIRECTORY
+    import os; 
+    if os.path.exists(RELATIVE_LOG_DIRECTORY) and log_directory is None: 
+        log_directory = RELATIVE_LOG_DIRECTORY
 
-        if log_directory is None: 
-            log_directory = DEFAULT_LOG_DIRECTORY
+    if log_directory is None: 
+        log_directory = DEFAULT_LOG_DIRECTORY
 
     return str(log_directory)
 
