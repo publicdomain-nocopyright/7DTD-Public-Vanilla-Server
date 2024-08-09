b0d0985c879d6f4e53c1188396c973fcfc4745cb
Feldwor <BoQsc@users.noreply.github.com>
2024-08-09 19:02:14 +0300

    update

---
+++ webserver/Webserver-testing/Webserver/Webserver_game_server_log_reader.py
@@ -0,0 +1,38 @@
+def get_latest_game_server_log_filepath(log_directory = None, relative_path = False):
+    from pathlib import Path; 
+    DEFAULT_LOG_DIRECTORY = r"C:\Program Files (x86)\Steam\steamapps\common\7 Days To Die\7DaysToDie_Data"
+    RELATIVE_LOG_DIRECTORY = Path(r'..\7DaysToDie_Data').resolve()
+
+    if log_directory is None: 
+        log_directory = DEFAULT_LOG_DIRECTORY
+
+    import os; 
+    if log_directory is DEFAULT_LOG_DIRECTORY:
+        if os.path.exists(RELATIVE_LOG_DIRECTORY): 
+            log_directory = RELATIVE_LOG_DIRECTORY
+
+    if log_directory == "DEFAULT_LOG_DIRECTORY":
+        log_directory = DEFAULT_LOG_DIRECTORY
+
+    if log_directory == "RELATIVE_LOG_DIRECTORY":
+        log_directory = RELATIVE_LOG_DIRECTORY
+        
+    print(log_directory)
+
+   # if relative_path:
+        
+    #if not relative_path:
+    #    DEFAULT_LOG_DIRECTORY = r"C:\Program Files (x86)\Steam\steamapps\common\7 Days To Die\7DaysToDie_Data"
+    #    
+    #import os 
+    #if log_directory is standard:
+    #    print("yes")
+    #    log_directory = os.path.abspath(__file__)
+    #
+    #
+    return {"game_server_log_file_path": ""}
+    pass
+
+
+if __name__ == "__main__":
+    get_latest_game_server_log_filepath("DEFAULT_LOG_DIRECTORY")
